e1e22c1edd1309bca1a3ac2da13a1ea0bd695e3b
matet <matet@pcdev16d1>
2020-09-02 15:46:37 +0200

    Updating the project with an UrlAnalyzer, Wordcounter, and HtmlObject class.

---
+++ urltask/src/main/java/com/trembi/urltask/HtmlObject.java
@@ -0,0 +1,36 @@
+/* 
+ * HtmlObject.java
+ *
+ * Copyright: SONY MUSIC ENTERTAINMENT 
+ * Created on: Sep 02, 2020
+ * Created by: Máté Trembickij
+ */
+package com.trembi.urltask;
+
+/**
+ *
+ * @author matet
+ */
+public class HtmlObject {
+    
+    private final String type;
+    private final String value;
+    
+    public HtmlObject(String type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public String getvalue() {
+        return value;
+    }
+    
+    public void printHtmlObject() {
+        System.out.println(type + ": " + value);
+    }
+    
+}
